dae1cf7f2aa79bb338abc4515e379b80466f9ab4
Nathan Chin <nathanchin@nathanchin.com>
2026-03-14 02:15:18 -0400

    Few more instructions

--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,7 @@ fn main() -> Result<()> {
 
     let mut boot_rom = [0; 256];
     File::open(&args.boot_rom)?.read_exact(&mut boot_rom);
+    debug!("{:#x?}", &boot_rom);
 
     let rom = {
         let mut buf = vec![];
@@ -97,14 +98,14 @@ impl<'rom> SimpleDmg<'rom> {
                 .ram
                 .get(usize::from(address - RAM_START_ADDRESS))
                 .copied()
-                .ok_or_else(|| eyre!("Error reading from ram at address {address}")),
+                .ok_or_else(|| eyre!("Error reading from ram at address {address:#x}")),
             // Echo RAM (mirror of C000–DDFF)
-            0xE000..0xFE00 => Err(eyre!("Invalid read at address {address}")),
+            0xE000..0xFE00 => Err(eyre!("Invalid read at address {address:#x}")),
             // Object attribute memory (OAM)
             0xFE00..0xFEA0 => todo!(),
             // Not Usable
-            0xFEA0..0xFF00 => Err(eyre!("Invalid read at address {address}")),
-            // I/O R
+            0xFEA0..0xFF00 => Err(eyre!("Invalid read at address {address:#x}")),
+            // I/O Registers
             0xFF00..0xFF80 => todo!(),
             // "High RAM (HRAM)"
             0xFF80..0xFFFF => todo!(),
@@ -139,7 +140,10 @@ impl<'rom> SimpleDmg<'rom> {
             // 16 KiB ROM Bank 01–NN
             0x4000..0x8000 => todo!(),
             // 8 KiB Video RAM (VRAM)
-            0x8000..0xA000 => todo!(),
+            0x8000..0xA000 => {
+                debug!("Write to VRAM at {:#x}", address);
+                Ok(())
+            }
             // 8 KiB External RAM
             0xA000..0xC000 => todo!(),
             // 8 KiB Work RAM (WRAM)
@@ -147,17 +151,20 @@ impl<'rom> SimpleDmg<'rom> {
                 *self
                     .ram
                     .get_mut(usize::from(address - RAM_START_ADDRESS))
-                    .ok_or_eyre(eyre!("Invalid write at address {address}"))? = data;
+                    .ok_or_eyre(eyre!("Invalid write at address {address:#x}"))? = data;
                 Ok(())
             }
             // Echo RAM (mirror of C000–DDFF)
-            0xE000..0xFE00 => Err(eyre!("Invalid write at address {address}")),
+            0xE000..0xFE00 => Err(eyre!("Invalid write at address {address:#x}")),
             // Object attribute memory (OAM)
             0xFE00..0xFEA0 => todo!(),
             // Not Usable
-            0xFEA0..0xFF00 => Err(eyre!("Invalid write at address {address}")),
-            // I/O R
-            0xFF00..0xFF80 => todo!(),
+            0xFEA0..0xFF00 => Err(eyre!("Invalid write at address {address:#x}")),
+            // I/O Registers
+            0xFF00..0xFF80 => {
+                debug!("Write to I/O registers at {address:#x}");
+                Ok(())
+            }
             // "High RAM (HRAM)"
             0xFF80..0xFFFF => todo!(),
             // Interrupt Enable register (IE)
@@ -166,9 +173,16 @@ impl<'rom> SimpleDmg<'rom> {
     }
 
     fn execute(&mut self) -> Result<()> {
+        let mut cb_prefix = false;
         loop {
             let opcode = self.read_pc_inc()?;
-            debug!("pc:{}, opcode:{:#x}", self.rf.pc, opcode);
+            debug!("pc:{:#x}, opcode:{:#x}", self.rf.pc, opcode);
+
+            if cb_prefix {
+                cb_prefix = false;
+                self.cb_operation(opcode);
+                continue;
+            }
 
             match opcode {
                 0x00 => trace!("NOP"),
@@ -231,18 +245,50 @@ impl<'rom> SimpleDmg<'rom> {
                     self.rf.sp = self.rf.sp.wrapping_add(1);
                 }
 
+                0x0e => {
+                    trace!("LD C,N");
+                    let n = self.read_pc_inc()?;
+                    self.rf.bc = u16::from_le_bytes([n, self.rf.bc.to_le_bytes()[1]])
+                }
+                0x3e => {
+                    trace!("LD A,N");
+                    let n = self.read_pc_inc()?;
+                    self.rf.a = n;
+                }
+
+                0x20 => {
+                    trace!("JR NZ,e");
+                    let e = self.read_pc_inc()?.cast_signed();
+                    if self.rf.f & FLAG_ZERO == FLAG_ZERO {
+                        self.rf.pc = u16::try_from(i32::from(self.rf.pc) + i32::from(e))?;
+                    }
+                }
+
                 0xaf => {
                     trace!("XOR A,A");
                     self.rf.a = 0;
                     self.rf.f = 0 | if self.rf.a == 0 { FLAG_ZERO } else { 0 };
                 }
 
+                0xcb => {
+                    cb_prefix = true;
+                    continue;
+                }
+
+                0xe2 => {
+                    trace!("LDH (C),A");
+                    let [c, _] = self.rf.bc.to_le_bytes();
+                    let address = u16::from_le_bytes([c, 0xff]);
+                    self.write(address, self.rf.a)?;
+                    debug!("Wrote data {:#x} to address {:#x}", self.rf.a, address);
+                }
+
                 0xfa => {
                     trace!("LD A,(nn)");
                     let nn = self.consume_16bit_direct()?;
-                    debug!("nn = {nn}");
+                    debug!("nn = {nn:#x}");
                     let data = self.read(nn)?;
-                    debug!("(nn) = {data}");
+                    debug!("(nn) = {data:#x}");
                     self.rf.a = data;
                 }
 
@@ -251,6 +297,22 @@ impl<'rom> SimpleDmg<'rom> {
         }
 
         Ok(())
+    }
+
+    fn cb_operation(&mut self, opcode: u8) {
+        match opcode {
+            0x7c => {
+                trace!("BIT 7,H");
+                if self.rf.hl.to_le_bytes()[1] & 0b10000000 == 0 {
+                    self.rf.f |= FLAG_ZERO;
+                } else {
+                    self.rf.f &= !FLAG_ZERO;
+                };
+                self.rf.f &= !FLAG_SUB;
+                self.rf.f |= FLAG_HALF_CARRY;
+            }
+            _ => todo!("CB-prefixed opcode not yet implemented: {opcode:#x}"),
+        }
     }
 }
 
